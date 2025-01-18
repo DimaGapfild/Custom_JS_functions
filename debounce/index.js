8f3f2dda55668fc76e3e093a7bd78602ea77c41e
Dima Karmazin <gapfild@gmail.com>
2025-01-18 21:21:38 +0200

    remove nodemone, implementation of first version of the function

--- debounce/index.js
+++ debounce/index.js
@@ -1,11 +1,25 @@
-function debounce(func, timeout = 300){
-    let timer;
-    return (...args) => {
-      clearTimeout(timer);
-      timer = setTimeout(() => { func.apply(this, args); }, timeout);
-    };
-  }
-  function saveInput(){
-    console.log('Saving data');
-  }
-  const processChange = debounce(() => saveInput());
+const textBoxContainer = document.querySelector("#text-box");
+const inputElement = document.querySelector("#custom-input-to-check-debounce-fn")
+
+function debounce(func, timeout = 1000) {
+  let timer;
+  return (...args) => {
+    clearTimeout(timer);
+    timer = setTimeout(() => {
+      func.apply(this, args);
+    }, timeout);
+  };
+}
+
+const clearInput = (input) => {
+  input.value = ""
+}
+
+function printInput() {
+  const newTextHolder = document.createElement("p")
+  newTextHolder.textContent = inputElement.value
+  textBoxContainer.append(newTextHolder)
+  clearInput(inputElement)
+}
+
+const processChange = debounce(() => printInput());
